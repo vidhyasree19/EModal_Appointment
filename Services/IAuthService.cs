92ad25132ca34e2f013b41ed0994a24b9469cb0c
Abdullah Mansuri <mansuriabdullah5080@gmail.com>
2024-09-19 10:28:47 +0530

    Auth Service Added

---
+++ Services/IAuthService.cs
@@ -0,0 +1,6 @@
+public interface IAuthService
+{
+    Task<bool> Register(AuthModel model);
+    Task<string> Login(AuthModel model);
+    Task<List<string>> GetUserRoles(string username); // New method to get user roles
+}
--- obj/Debug/net8.0/Appointment_Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Appointment_Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Appointment_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff28dcf90f564b98c454577b2445cd2e94856b9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852dfdf86b8e02ca2a1500375b7c79319037886d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Appointment_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Appointment_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
