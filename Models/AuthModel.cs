9b28e99659c328957892f7f250f44fe2dc7573bb
vidhyasree19 <133778975+vidhyasree19@users.noreply.github.com>
2024-09-23 10:02:20 +0530

    Delete Models directory

--- Models/AuthModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AuthlApi.Models
-{
-public class AuthModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Role { get; set; } // Optional for registration
-}
-}
--- Models/User.cs
+++
@@ -1,7 +0,0 @@
- public class User
-    {
-        public int Id { get; set; } // Primary Key
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; } // Role for the user
-    }
