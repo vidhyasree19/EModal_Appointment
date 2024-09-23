8bd4dfa092ec08bb2c583820920aeb846457ae8c
vidhyasree19 <133778975+vidhyasree19@users.noreply.github.com>
2024-09-23 10:03:15 +0530

    Delete Services directory

--- Services/IAuthService.cs
+++
@@ -1,7 +0,0 @@
-using AuthlApi.Models;
-public interface IAuthService
-{
-    Task<bool> Register(AuthModel model);
-    Task<string> Login(AuthModel model);
-    Task<List<string>> GetUserRoles(string username); // New method to get user roles
-}
